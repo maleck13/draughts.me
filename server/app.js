121b5dc3c7033ef05ada7a0cc77d1aa8fcaabc12
Craig Brookes <craigbrookes@gmail.com>
2012-03-06 22:31:22 +0000

    added express

--- server/app.js
+++ server/app.js
@@ -11,7 +11,7 @@ var app = module.exports = express.createServer();
 // Configuration
 
 app.configure(function(){
-  app.set('views', '../client/views');
+  app.set('views', __dirname + '../client/views');
   app.set('view engine', 'jade');
   app.use(express.bodyParser());
   app.use(express.methodOverride());
