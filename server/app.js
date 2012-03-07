921155ddf3ec8a2397ee0b1b79173d9626050731
Craig Brookes <craigbrookes@gmail.com>
2012-03-07 21:27:58 +0000

    basic setup of game server made

--- server/app.js
+++ server/app.js
@@ -5,18 +5,21 @@
 
 var express = require('express')
   , routes = require('./routes')
+  , socketserver = require('./lib/socketserver.js')
+  , fs = require('fs');
 
 var app = module.exports = express.createServer();
 
 // Configuration
 
 app.configure(function(){
-  app.set('views', __dirname + '../client/views');
+    
+  app.set('views', './client/views');
   app.set('view engine', 'jade');
   app.use(express.bodyParser());
   app.use(express.methodOverride());
   app.use(app.router);
-  app.use(express.static(__dirname + '../client/public'));
+  app.use(express.static('./client/public'));
 });
 
 app.configure('development', function(){
@@ -30,6 +33,10 @@ app.configure('production', function(){
 // Routes
 
 app.get('/', routes.index);
+
+
+//start sockets
+socketserver.startSocketServer(app);
 
 app.listen(8006);
 console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
