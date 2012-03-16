8e6954e616474eca229380602697f00085fb645b
Phil brookes <phil.brookes@gmail.com>
2012-03-16 22:22:48 +0000

    first moves on JS rendering engine

--- server/app.js
+++ server/app.js
@@ -13,13 +13,13 @@ var app = module.exports = express.createServer();
 // Configuration
 app.configure(function(){
     
-  app.set('views', './client/views');
+  app.set('views', '../client/views');
   app.set('view engine', 'jade');
   app.use(express.bodyParser());
   app.use(express.methodOverride());
   app.use(app.router);
 
-  app.use(express.static('./client/public'));
+  app.use(express.static('../client/public'));
 
 });
 
