cb62279b68b7f90e496b9f27438f7caee02db99e
Craig Brookes <craigbrookes@gmail.com>
2012-03-19 14:22:41 +0000

    added config and exceptions

---
+++ test/test_game_validation.js
@@ -0,0 +1,19 @@
+/**
+ * Created by JetBrains WebStorm.
+ * User: craigbrookes
+ * Date: 19/03/2012
+ * Time: 14:16
+ * To change this template use File | Settings | File Templates.
+ */
+var assert = require('assert');
+var gameMan = require('../server/lib/gamemanager.js');
+
+describe("verify a game is ok", function (){
+   it("should create a game and preform a move without validation error", function(){
+       
+   });
+    
+   it("should create a game and perform an illigal move causing validation error", function () {
+       
+   }); 
+});
