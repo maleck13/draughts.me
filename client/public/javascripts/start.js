c2a66a680238300de31ea22e21b4962326b22739
Phil brookes <phil.brookes@gmail.com>
2012-03-20 21:53:43 +0000

    animation stress test

--- client/public/javascripts/start.js
+++ client/public/javascripts/start.js
@@ -2,9 +2,19 @@ $(document).ready(
     function(){
         Engine.init("myDrawing");
         var factory = PieceFactory();
-        var position = new Position(50, 50);
-        var piece = factory.createPiece("#ff0000", position);
-        Engine.addObject(piece);
+        var position;
+        var piece;
+        for(j=1;j<=7;j++)
+        {
+            for(i=1;i<=15;i++)
+            {
+                position = new Position(i*50, j*50);
+                piece = factory.createPiece("#ff0000", position);
+                
+                Engine.addObject(piece);
+            }
+        }
+        
     }
 );
 
